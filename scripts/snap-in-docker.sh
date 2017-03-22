094d2eee6467f39b7d95d0b18ebb3cb0f62b1de3
Leo Arias <leo.arias@canonical.com>
2017-03-22 19:41:54 +0000

    Rename variables

--- scripts/snap-in-docker.sh
+++ scripts/snap-in-docker.sh
@@ -12,14 +12,14 @@ set -ev
 source="$(cat $1/snap/snapcraft.yaml | grep source: | head -n 1 | awk '{printf $2}')"
 repo="$(echo $source | sed 's|^.*github\.com/||')"
 wget https://api.github.com/repos/$repo/releases/latest
-last_committed_tag="$(jq .tag_name latest)"
+last_released_tag="$(jq .tag_name latest)"
 docker run -v "${HOME}":/root -v $(pwd):$(pwd) snapcore/snapcraft sh -c "apt update && apt install -y snapcraft && cd $(pwd)/$1 && ((snapcraft status $1 || echo "none") > status)"
-last_released_tag="$(awk '$1 == "beta" { print $2 }' $1/status)"
+last_released_snap="$(awk '$1 == "beta" { print $2 }' $1/status)"
 
-if [ "${last_committed_tag}" != "${last_released_tag}" ]; then
+if [ "${last_released_tag}" != "${last_released_snap}" ]; then
   # Build using the latest tag.
-  sed -i "0,/source-tag/s/source-tag:.*$/source-tag: '"$last_committed_tag"'/g" $1/snap/snapcraft.yaml
-  sed -i "s/version:.*$/version: '"$last_committed_tag"'/g" $1/snap/snapcraft.yaml
+  sed -i "0,/source-tag/s/source-tag:.*$/source-tag: '"$last_released_tag"'/g" $1/snap/snapcraft.yaml
+  sed -i "s/version:.*$/version: '"$last_released_tag"'/g" $1/snap/snapcraft.yaml
   # Set the stable grade to be able to move it to the candidate and stable channels.
   sed -i "s/grade:.*$/grade: stable/g" $1/snap/snapcraft.yaml
 fi
