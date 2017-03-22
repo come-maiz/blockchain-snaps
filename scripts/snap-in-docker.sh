1f1a10926982efe951373ea9575e96c72991e801
Leo Arias <leo.arias@canonical.com>
2017-03-22 12:08:33 -0600

    Release the latest tag to edge, unless it is in beta (#8)

--- scripts/snap-in-docker.sh
+++ scripts/snap-in-docker.sh
@@ -8,4 +8,20 @@
 
 set -ev
 
+# Check if the latest tag is in the beta channel.
+tmp_dir="$(mktemp -d)"
+source="$(cat $1/snap/snapcraft.yaml | grep source: | head -n 1 | awk '{printf $2}')"
+git clone "${source}" "${tmp_dir}"
+last_committed_tag="$(git -C "${tmp_dir}" describe --tags --abbrev=0)"
+docker run -v "${HOME}":/root -v $(pwd):$(pwd) snapcore/snapcraft sh -c "apt update && apt install -y snapcraft && cd $(pwd)/$1 && ((snapcraft status $1 || echo "none") > status)"
+last_released_tag="$(awk '$1 == "beta" { print $2 }' $1/status)"
+
+if [ "${last_committed_tag}" != "${last_released_tag}" ]; then
+  # Build using the latest tag.
+  sed -i "0,/source-tag/s/source-tag:.*$/source-tag: '"$last_committed_tag"'/g" $1/snap/snapcraft.yaml
+  sed -i "s/version:.*$/version: '"$last_committed_tag"'/g" $1/snap/snapcraft.yaml
+  # Set the stable grade to be able to move it to the candidate and stable channels.
+  sed -i "s/grade:.*$/grade: stable/g" $1/snap/snapcraft.yaml
+fi
+
 docker run -v "$(pwd)":/cwd snapcore/snapcraft sh -c "apt update && apt upgrade -y && cd /cwd && ./scripts/snap.sh $1"
